069c2c7d37bded2b7b5094cf661292295d99af32
Asna <asna.ansari@gmail.com>
2020-07-24 11:00:00 -0700

    one flat table with white student rates and all races rates

--- national_rates_table.sql
+++ national_rates_table.sql
@@ -29,6 +29,9 @@ SELECT SUM("SCH_DISCWODIS_MULTOOS_WH_M") / SUM(NULLIF("SCH_ENR_WH_M", 0)) as ntl
 , SUM("SCH_APENR_WH_M") / SUM(NULLIF("SCH_ENR_WH_M", 0)) as ntl_WH_M_APENR_rate
 , SUM("SCH_APENR_WH_F") / SUM(NULLIF("SCH_ENR_WH_F", 0)) as ntl_WH_F_APENR_rate
 , SUM("SCH_APENR_WH_F" + "SCH_APENR_WH_M") / SUM(NULLIF("SCH_ENR_WH_F", 0) + NULLIF("SCH_ENR_WH_M", 0)) as ntl_WH_both_genders_APENR_rate
+
+# metrics for all races
+
 , SUM("TOT_DISCWODIS_MULTOOS_M") / SUM(NULLIF("TOT_ENR_M", 0)) as ntl_all_races_M_WODIS_MULTOOS_rate
 , SUM("TOT_DISCWODIS_MULTOOS_F") / SUM(NULLIF("TOT_ENR_F", 0)) as ntl_all_races_F_WODIS_MULTOOS_rate
 , SUM("TOT_DISCWODIS_MULTOOS_F" + "TOT_DISCWODIS_MULTOOS_M") / SUM((NULLIF("TOT_ENR_F", 0) + NULLIF("TOT_ENR_M", 0))) as ntl_all_races_both_genders_WODIS_MULTOOS_rate
