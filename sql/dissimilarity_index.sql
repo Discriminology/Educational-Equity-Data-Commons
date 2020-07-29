c057b40a4e0b6c19e1bbbc8927ec927b6ccb3833
Asna <asna.ansari@gmail.com>
2020-07-29 12:43:09 -0700

    count districts with 100 students in 3 or more racial categories

--- sql/dissimilarity_index.sql
+++ sql/dissimilarity_index.sql
@@ -1,13 +1,46 @@
 -- Number of 15/16 districts with enrollment of at least 1000 and more than 5 schools
--- and at lesat 100 students in 3 or more racial categories
+-- and at least 100 students in 3 or more racial categories
 
 select count(distinct "LEAID")
 FROM districts
 WHERE
 "YEAR" = '2015-16'
-AND "TOTAL_ENROLLMENT" >= 1000
-AND "TOTAL_SCHOOLS" >= 5
-AND (("SCH_ENR_HI_F" + "SCH_ENR_HI_M") > 100)
-AND (("SCH_ENR_WH_F" + "SCH_ENR_WH_M") > 100)
-AND (("SCH_ENR_BL_F" + "SCH_ENR_BL_M") > 100)
-;
+AND "TOTAL_ENROLLMENT" > 1000
+AND "TOTAL_SCHOOLS" > 5
+AND (CASE WHEN (
+				("SCH_ENR_HI_F" + "SCH_ENR_HI_M") > 100
+					)
+	THEN 1 ELSE 0 end
+		) +
+	(CASE WHEN (
+			("SCH_ENR_BL_F" + "SCH_ENR_BL_M") > 100
+				)
+	THEN 1 ELSE 0 end
+	+
+	(CASE WHEN (
+			("SCH_ENR_WH_F" + "SCH_ENR_WH_M") > 100
+				)
+	THEN 1 ELSE 0 end)
+	+
+	(CASE WHEN (
+			("SCH_ENR_AS_F" + "SCH_ENR_AS_M") > 100
+				)
+	THEN 1 ELSE 0 end)
+	+
+	(CASE WHEN (
+			("SCH_ENR_AM_F" + "SCH_ENR_AM_M") > 100
+				)
+	THEN 1 ELSE 0 end)
+	+
+	(CASE WHEN (
+			("SCH_ENR_TR_F" + "SCH_ENR_TR_M") > 100
+				)
+	THEN 1 ELSE 0 end)
++
+	(CASE WHEN (
+			("SCH_ENR_HP_F" + "SCH_ENR_HP_M") > 100
+				)
+	THEN 1 ELSE 0 end)
+
+		) >= 3
+	;
