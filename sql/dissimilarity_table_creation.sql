a6e7beba825a43683e2f1581792e97af4165f982
Asna <asna.ansari@gmail.com>
2020-07-30 15:29:09 -0700

    create statement for table with dissimilarity scores

---
+++ sql/dissimilarity_table_creation.sql
@@ -0,0 +1,17 @@
+CREATE TABLE classified_dissimilarity_index (
+    "LEAID" text,
+    "YEAR" text,
+    d_wh_bl double precision,
+    d_wh_as double precision,
+    d_wh_am double precision,
+    d_wh_tr double precision,
+    d_wh_hp double precision,
+    d_wh_hi double precision,
+    classified_d_wh_bl text, 
+    classified_d_wh_as text,
+   	classified_d_wh_am text,
+   	classified_d_wh_tr text,
+   	classified_d_wh_hp text,
+    classified_d_wh_hi text
+);
+
