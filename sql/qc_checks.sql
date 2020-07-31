1b28b190567d811b388affbbf657be8c372abbda
Asna <asna.ansari@gmail.com>
2020-07-31 10:20:37 -0700

    more qc checks on categorization

--- sql/qc_checks.sql
+++ sql/qc_checks.sql
@@ -3,4 +3,9 @@
 select * from classified_dissimilarity_index where d_wh_bl > 0.5;
 select * from classified_dissimilarity_index where d_wh_bl < 0.2;
 select * from classified_dissimilarity_index where d_wh_as > 0.5;
+
 select * from classified_dissimilarity_index where d_wh_am > 0.5;
+
+
+select * from classified_dissimilarity_index where d_wh_am < 0.2 and classified_d_wh_am !='low';
+-- Should return 0 results
